9b83caac2f23caa656f09e7430760de3b74662ec
iti98 <etienne98.ch@gmail.com>
2022-10-28 15:24:13 +0200

    Exo 2 partie 2 finit

--- Models/ShoppingCart.php
+++ Models/ShoppingCart.php
@@ -4,27 +4,34 @@ namespace Models;
 
 class ShoppingCart
 {
-    private $itemList = array();
-    private static $nbPaniers = 0;
+    private array $itemList = array();
+    private static int $nbPaniers = 0;
     private $id;
+    private $totalWeight;
 
     public function __construct()
     {
         $this->id = self::$nbPaniers;
         self::$nbPaniers++;
+        $this->totalWeight = 0;
     }
 
-    public function addItem($item)
+    public function addItem(Item $item)
     {
-        $this->itemList[] = $item;
-
+        if ($this->checkWeight($item)) {
+            $this->itemList[] = $item;
+        }
         return $this->itemList;
     }
 
-    public function removeItem($item)
+    public function removeItem(Item $item)
     {
-        $this->itemList;
-        return $this->itemList;
+        $index = array_search($item, $this->itemList);
+        if ($index !== false) {
+            $this->totalWeight -= $this->itemList[$index]->getWeight();
+            unset($this->itemList[$index]);
+        }
+        return $index !== false ? $index : false;
     }
 
     public function itemCount()
@@ -35,15 +42,24 @@ class ShoppingCart
     public function totalPrice()
     {
         $total = 0;
-        foreach ($this->itemList as $item ) {
+        foreach ($this->itemList as $item) {
             $total += $item->getPrice();
         }
         return $total;
     }
 
+    private function checkWeight(Item $item, int $maxWeight = 1000)
+    {
+        $isGood = ($this->totalWeight + $item->getWeight()) <= $maxWeight;
+        if ($isGood) {
+            $this->totalWeight += $item->getWeight();
+        }
+        return $isGood;
+    }
+
     /**
      * Get the value of id
-     */ 
+     */
     public function getId()
     {
         return $this->id;
