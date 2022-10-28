edef40f59f31831a253fcb2ac5720cfabd4c052b
iti98 <etienne98.ch@gmail.com>
2022-10-28 15:57:53 +0200

    Ijust prefer simple quotes to double quotes

--- index.php
+++ index.php
@@ -4,19 +4,19 @@ use Models\ShoppingCart;
 use Models\Item;
 
 require_once('autoload.php');
-// $item = new Item("corn flakes", 500, 250);
+// $item = new Item('corn flakes', 500, 250);
 // var_dump($item->getPrice());
 // var_dump($item->getName());
 
 // $item->displayInfo();
-// $item = new Item("chewing gum", 100, 10);
+// $item = new Item('chewing gum', 100, 10);
 // $item->displayInfo();
 
 
 // $shopping = new ShoppingCart();
-// var_dump($shopping->addItem(new Item("chewing gum", 403, 10)));
-// var_dump($shopping->addItem(new Item("Bueno", 600, 25)));
-// var_dump($shopping->addItem(new Item("Pingui", 250, 25)));
+// var_dump($shopping->addItem(new Item('chewing gum', 403, 10)));
+// var_dump($shopping->addItem(new Item('Bueno', 600, 25)));
+// var_dump($shopping->addItem(new Item('Pingui', 250, 25)));
 
 // var_dump($shopping->itemCount());
 
@@ -24,19 +24,19 @@ require_once('autoload.php');
 
 $shopping = new ShoppingCart();
 
-$item = new Item("corn flakes", 500, 250);
+$item = new Item('corn flakes', 500, 250);
 $shopping->addItem($item);
-$shopping->addItem(new Item("Bueno", 600, 250));
-$shopping->addItem(new Item("Pingui", 250, 250));
-$shopping->addItem(new Item("Country", 250, 250));
+$shopping->addItem(new Item('Bueno', 600, 250));
+$shopping->addItem(new Item('Pingui', 250, 250));
+$shopping->addItem(new Item('Country', 250, 250));
 // // Work
-// $shopping->addItem(new Item("Delice", 250, 250));
+// $shopping->addItem(new Item('Delice', 250, 250));
 // // Don't add (so it work)
 // $shopping->removeItem($item);
 // // Remove correctly (so it work)
-// $shopping->addItem(new Item("Bueno white", 250, 300));
+// $shopping->addItem(new Item('Bueno white', 250, 300));
 // // Don't add (so it work)
-// $shopping->addItem(new Item("Bueno white", 250, 250));
+// $shopping->addItem(new Item('Bueno white', 250, 250));
 // // Add (so it work)
 // var_dump($shopping);
 // // Fin exo 2.2
